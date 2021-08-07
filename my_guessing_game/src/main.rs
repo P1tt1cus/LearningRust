1426d95cfb8848cf509802fef9b8776b9cf72773
P1tt1cus <tomwpitt1@gmail.com>
2021-08-07 19:04:25 +1000

    Added variables, my_guessing_game

---
+++ my_guessing_game/src/main.rs
@@ -0,0 +1,65 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+
+fn main() {
+    
+    println!("Guess the number!"); 
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // println!("The secret number is: {}", secret_number);
+
+    let mut attempts = 0;
+
+    let mut max_tries= 3;
+
+    let mut attempts_left = attempts - max_tries;
+
+    loop {
+        
+        println!("Please input your guess:");
+
+        // Declares a mutable variable, variables are immutable by default
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // Expect call, attempts to convert the string into u32 or errors 
+        // let guess: u32 = guess.trim().parse().expect("Please type a number, u fek!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        attempts_left = max_tries - attempts;
+
+        match attempts.cmp(&max_tries) {
+            Ordering::Less => println!("You have {} attempts left", attempts_left),
+            Ordering::Equal => {
+                println!("You have run out of attempts!"); 
+                break;
+            },
+            Ordering::Greater => println!("wut"),
+        };
+        println!("You guessed: {}", guess);
+
+        attempts = attempts + 1;
+
+        println!("Attempts: {}", attempts);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        };
+    }
+}
+
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,14 @@
 fn main() {
 
-    
+    // // Declaring a variable as mutable, so it can be changed
+    // let mut x = 5;
 
+    // println!("The value of x is: {}", x);
+
+    // // Changing a mutable variable
+    // x = 6;
+
+    // println!("The value of x is: {}", x);
+
+    let x = 5; 
 }
